39b5a28458cff10646464ab46c5973635665da16
Frank Pommerening <frank.pommerening@axp-consulting.de>
2017-03-12 12:01:57 +0100

    Init WebApp Sample

---
+++ WebApp/WebApp/HomeModule.cs
@@ -0,0 +1,10 @@
+﻿namespace FP.DotnetInTheBox.Sample.WebApp
+{
+    public class HomeModule : Nancy.NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => "Hallo Test");
+        }
+    }
+}
